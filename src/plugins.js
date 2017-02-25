af113d510c5affef1c55e98b6a45291d38323437
anthonny <anthonny.querouil@gmail.com>
2017-02-25 08:15:18 +0100

    feat(migrate): rework

--- src/plugins.js
+++ src/plugins.js
@@ -1,32 +1,46 @@
-import Q from 'q';
+import Promise from 'bluebird'
+import _ from 'lodash'
 
-let listeners = {};
+const listeners = {
+  '*': []
+}
 
-function fire (event, opts = {state: {}, data:{}}) {
-  return (listeners[event] || [])
+function fire (event, opts = {rootState: {}, currentState: {}, payload: {}}) {
+  const nextState = opts.nextState || _.cloneDeep(opts.currentState)
+  const fullyOpts = Object.assign(opts, {event, nextState})
+
+  listeners[event] = listeners[event] || []
+  if (!listeners[event].length) {
+    console.info(`No plugin have a callback for the event ${event}`)
+    listeners[event].push(_opts => {
+      console.log(`Default event function for ${event}`, _opts)
+      return _opts
+    })
+  }
+
+  return (listeners[event].concat(listeners['*']))
   .reduce((memo, promise) => {
-    return memo.then(promise);
-  }, Q(opts))
+    return memo.then(promise)
+  }, Promise.resolve(fullyOpts))
 }
 
 function on (event, callback) {
-  (listeners[event] || (listeners[event] = [])).push(callback);
+  (listeners[event] || (listeners[event] = [])).push(callback)
 }
 
 function register (...plugins) {
-
-  let hubpress = {
+  const context = {
     on,
     fire
-  };
+  }
 
-  plugins.forEach( plugin => plugin(hubpress))
+  plugins.forEach(plugin => plugin(context))
 
-  return hubpress;
+  return context
 }
 
 export default {
   register,
   fire,
   on
-};
+}
